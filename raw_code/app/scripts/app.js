a357ae520f1974cdfb927f8938a5838b4bf3d4e3
vluna <vluna.0293@gmail.com>
2015-04-26 15:21:59 -0700

    separated folders

---
+++ raw_code/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name iat381FinalProjectCheeasonApp
+ * @description
+ * # iat381FinalProjectCheeasonApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('iat381FinalProjectCheeasonApp', [
+    'ngAnimate',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngAside',
+    'ui.bootstrap',
+    'indexedDB',
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/weather.html',
+        controller: 'WeatherCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
